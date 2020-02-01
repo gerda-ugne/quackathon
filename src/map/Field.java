fea73ee012d2a862ccc623902e3045047d3e40ee
MiklosMayer <mmayer@dundee.ac.uk>
2020-02-01 16:15:23 +0000

    Backup commit

--- src/map/Field.java
+++ src/map/Field.java
@@ -2,19 +2,57 @@ package map;
 
 public class Field {
 
-    private boolean canGo;
+    private boolean right;
+    private boolean bottom;
+    private boolean left;
+    private boolean top;
     private boolean hasItem;
 
-    public Field(boolean canGo, boolean hasItem) {
-        this.canGo = canGo;
-        this.hasItem = hasItem;
+    public Field() {
+        right = false;
+        bottom = false;
+        left = false;
+        top = false;
+        hasItem = false;
     }
 
-    public boolean isCanGo() {
-        return canGo;
+    public boolean isRight() {
+        return right;
     }
 
-    public boolean isHasItem() {
+    public void setRight(boolean right) {
+        this.right = right;
+    }
+
+    public boolean isBottom() {
+        return bottom;
+    }
+
+    public void setBottom(boolean bottom) {
+        this.bottom = bottom;
+    }
+
+    public boolean isLeft() {
+        return left;
+    }
+
+    public void setLeft(boolean left) {
+        this.left = left;
+    }
+
+    public boolean isTop() {
+        return top;
+    }
+
+    public void setTop(boolean top) {
+        this.top = top;
+    }
+
+    public boolean hasItem() {
         return hasItem;
+    }
+
+    public void setHasItem(boolean hasItem) {
+        this.hasItem = hasItem;
     }
 }
