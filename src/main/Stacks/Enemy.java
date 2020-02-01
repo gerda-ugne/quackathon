7f9e2973419d035b6206b5719facfd55f29b0743
MiklosMayer <mmayer@dundee.ac.uk>
2020-02-01 21:19:45 +0000

    Update Main in Stacks

--- src/main/Stacks/Enemy.java
+++ src/main/Stacks/Enemy.java
@@ -1,3 +1,4 @@
+package main.Stacks;
 
 public class Enemy {
 
--- src/main/Stacks/Main.java
+++ src/main/Stacks/Main.java
@@ -16,7 +16,7 @@ public class Main {
 		// TODO Auto-generated constructor stub
 		map = new Map();
 		enemy = new Enemy();
-		player = new Player(null, 0, 0, 0, 0);
+		player = new Player();
 	}
 
 	public static void main(String[] args) {
@@ -34,70 +34,110 @@ public class Main {
 	
 	public void showCombatOptions()
 	{
-		System.out.println("1. Attack");
-		System.out.println("2. Use a special attack");
-		System.out.println("3. Use a HP potion");
-		System.out.println("4. Use a MP potion");
+		System.out.println("1. Peck the human with your beak.");
+		System.out.println("2. Unleash a flapping attack [Special attack]");
+		System.out.println("3. Have a crunchy worm with a rock [Heal up]");
+		System.out.println("4. Use your wax to restore your powers [Restore MP]");
 		System.out.println("0. Flee");
 		
 	}
 	
 	public void combat()
 	{
-		System.out.println("You have encountered an enemy!");
+		System.out.println("You have encountered a human, and they seem to be polluting the lake!");
 		Scanner s = new Scanner(System.in);
 		String userInput;
 		String enemyInput;
 		
-		//Only one move of the same kind permitted per turn
-		int attackLimit = 0;
-		int specialAttackLimit=0;
-		int healHPLimit = 0;
-		int healMPLimit = 0;
+		String recordedUserMoves = "";
+		String recordedEnemyMoves = "";
+
+		//Records damage enemy and player do each turn
+		int enemyDamage =  0;
+		int playerDamage = 0;
+
+		//Retry if input is invalid
+		boolean retry=false;
 		
 		do {
 			
 			//Player has their turn first
 			for (int i=0; i<2; i++)
 			{
-				System.out.println("Please choose your next move:");
-				if(i == 1) 	System.out.println("You have 1 move left.");
-				else System.out.println("You have " + i + " moves left.");
-				
-				showCombatOptions();
-				
-				switch(userInput)
+				do
 				{
-				case "1": player.attack();attackLimit++; break;
-				case "2": player.specialAttack(); specialAttackLimit++; break;
-				case "3": player.healHP(); healHPLimit++; break;
-				case "4": player.healMP(); healMPLimit++; break;
-				case "0": player.flee();break;
-				}
-				
+					System.out.println("Please choose your next move:");
+					if(i == 1) 	System.out.println("You have 1 move left.");
+					else System.out.println("You have " + i + " moves left.");
+
+					userInput = s.nextLine();
+
+					showCombatOptions();
+					retry = false;
+
+					switch(userInput)
+					{
+					case "1": recordedUserMoves = recordedUserMoves + "1";break;
+					case "2": recordedUserMoves = recordedUserMoves + "2"; break;
+					case "3": recordedUserMoves = recordedUserMoves + "3"; break;
+					case "4": recordedUserMoves = recordedUserMoves + "4"; break;
+					case "0": player.flee();break;
+					default: System.out.println("Please try again - wrong user input."); retry = true; break;
+					}
+
+					//separating input with a space
+					recordedUserMoves = recordedUserMoves + " ";
+
+				} while (retry=true);
 			}
 			
-			//Enemy has their turn		
+			//Player choices are executed, damage recorded
+			playerDamage = player.execute(recordedUserMoves);
+
+			//Enemy health is deduced after player moves
+			enemy.setHealth(enemy.getEnemyHealth()-playerDamage);
+			if(enemy.isEnemyAlive() == true) break;
+
+			//Enemy has their turn
 			for (int i=0; i<2; i++)
 			{
-				System.out.println("Enemy has their turn!");
+				System.out.println("Human has their turn!");
 				int random = (int )(Math.random() * 4 + 1);
 				enemyInput = Integer.toString(random);
 				
 				switch(enemyInput)
 				{
-				case "1": enemy.attack();attackLimit++; break;
-				case "2": enemy.specialAttack(); specialAttackLimit++; break;
-				case "3": enemy.healHP(); healHPLimit++; break;
-				case "4": enemy.healMP(); healMPLimit++; break;
+				case "1": recordedEnemyMoves = recordedEnemyMoves + "1";break;
+				case "2": recordedEnemyMoves = recordedEnemyMoves + "2"; break;
+				case "3": recordedEnemyMoves = recordedEnemyMoves + "3"; break;
+				case "4": recordedEnemyMoves = recordedEnemyMoves + "4"; break;
 				}
 				
+				//Separating input with a space
+				recordedEnemyMoves = recordedEnemyMoves + " ";
+
 			}
 			
-			
+			//Enemy choices are executed, damage recorded
+			enemyDamage = enemy.execute(recordedEnemyMoves);
+
+			//Player's health is deduced after enemy moves
+			player.setHealth(player.getHealth()- enemyDamage);
+
 			
 		} while(enemy.isEnemyAlive() == false || player.isPlayerAlive() == false);
+
+
+	if(enemy.isEnemyAlive() == false)
+	{
+		System.out.println("You have scared the polluter human and they have ran away! Good job.");
 	}
+	else if(player.isPlayerAlive() == false)
+	{
+		System.out.println("The polluter has drowned you in trash and you have lost all your items. You must retreat for now.");
+	}
+
+  }
 
 }
 
